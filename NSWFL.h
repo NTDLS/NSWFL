351e96b05762e37cdab3504cca90c02fd8654961
NTDLS <JPatterson@NTDLS.com>
2018-04-09 15:25:56 -0400

    Add files via upload

---
+++ NSWFL.h
@@ -0,0 +1,33 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//  Copyright © NetworkDLS 2010, All rights reserved
+//
+// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF 
+// ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO 
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A 
+// PARTICULAR PURPOSE.
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#ifndef _NSWFL_H_
+#define _NSWFL_H_
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "../Debug/Debug.H"
+#include "../InsecureInterface/ISecInt.H"
+
+#include "NSWFL_Types.H"
+#include "NSWFL_Conversion.H"
+#include "NSWFL_DateTime.H"
+#include "NSWFL_KeyGeneration.H"
+#include "NSWFL_ListBox.H"
+#include "NSWFL_Math.H"
+#include "NSWFL_Memory.H"
+#include "NSWFL_Registry.H"
+#include "NSWFL_String.H"
+#include "NSWFL_System.H"
+#include "NSWFL_Windows.H"
+#include "NSWFL_File.H"
+#include "NSWFL_Menu.H"
+#include "NSWFL_ListView.H"
+#include "NSWFL_Graphics.H"
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#endif
